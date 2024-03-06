1e79bbb014859187d871abc17f28597881ca900d
Fehu18 <145318881+Fehu18@users.noreply.github.com>
2024-03-06 15:24:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -73,22 +73,4 @@ resource "azurerm_app_service_plan" "app_service_plan" {
 
 data "azurerm_client_config" "current" {}
 
-output "vnet_id" {
-  value = azurerm_virtual_network.vnet.id
-}
 
-output "subnet1_id" {
-  value = azurerm_subnet.subnet1.id
-}
-
-output "subnet2_id" {
-  value = azurerm_subnet.subnet2.id
-}
-
-output "kv_id" {
-  value = azurerm_key_vault.kv.id
-}
-
-output "app_service_id" {
-  value = azurerm_app_service.app_service.id
-}
